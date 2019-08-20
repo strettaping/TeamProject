e6f2127f5990d3249346cb959d1bd9ff3a4e5f8a
soldesk <soldesk@soldesk-PC>
2019-08-20 17:07:21 +0900

    190820 Commit
    ...

--- project3/src/main/java/com/spring/service/StoreSvc.java
+++ project3/src/main/java/com/spring/service/StoreSvc.java
@@ -6,7 +6,7 @@ import com.spring.vo.StoreVO;
 
 public interface StoreSvc {
 	public List<StoreVO> listCategory(String category);
-	public List<StoreVO> listSellID(String sellID);
+	public List<StoreVO> listsPNum(String sPNumber);
 	public void write(StoreVO svo);
 	public void modify(Integer sID);
 	public void remove(Integer sID);
